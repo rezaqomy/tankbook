14ffaa073b127cf7cfe3fc241dcbf3d6ae6e971b
reza <rezaqomyalasl@gmail.com>
2025-01-28 19:29:05 +0330

    create method for get by id user name email and create the get current user method

--- src/auth/service.py
+++ src/auth/service.py
@@ -1,13 +1,30 @@
-from typing import Optional
-from fastapi import HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+
+from typing import Annotated, Optional
+from fastapi import Depends, HTTPException, Request, status
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from sqlalchemy.exc import NoResultFound
+
+from datetime import datetime, timedelta, UTC
+
+from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND
+
+from src.database.core import DbSession, get_db
+from .models import (UserCreate, User, TokenData)
+from src.config import settings
 
 
-from src.database.core import DbSession
-from .models import (UserCreate, User)
-from src.enums import UserRoles
+JWT_EXP = settings.JWT_EXP
+JWT_ALG = settings.JWT_ALG
+JWT_SECRET = settings.JWT_SECRET
 
+
+async def get_by_id(*, db_session: DbSession, id: int) -> Optional[User]:
+    """return a user object based on user id"""
+    stmt = select(User).where(User.id == id)
+    result = await db_session.execute(stmt)
+    return result.scalars().one_or_none()
 
 
 async def get_by_email(*, db_session: DbSession, email: str) -> Optional[User]:
@@ -15,6 +32,7 @@ async def get_by_email(*, db_session: DbSession, email: str) -> Optional[User]:
     stmt = select(User).where(User.email == email)
     result = await db_session.execute(stmt)
     return result.scalars().one_or_none()
+
 
 
 async def get_by_username(*, db_session: DbSession, username: str) -> Optional[User]:
@@ -29,7 +47,6 @@ async def get_by_phone_number(*, db_session: DbSession, phone_number: str) -> Op
     stmt = select(User).where(User.phone_number == phone_number)
     result = await db_session.execute(stmt)
     return result.scalars().one_or_none()
-
 
 
 async def create_user(user: UserCreate, db_session: DbSession) -> Optional[User]:
@@ -60,3 +77,34 @@ async def create_user(user: UserCreate, db_session: DbSession) -> Optional[User]
     await db_session.commit()
     await db_session.refresh(db_user)
     return db_user
+
+
+
+async def get_current_user(request: Request, db_session: AsyncSession = Depends(get_db)) -> User:
+    """Attempts to get the current user using JWT token."""
+    token = request.headers.get("Authorization")
+    if not token:
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED, 
+            detail="Authorization header missing"
+        )
+
+    token = token.replace("Bearer ", "")
+    print(token)
+    payload = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALG])
+    print(payload)
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALG])
+        username = payload.get("username")
+        if username is None:
+            raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    except JWTError:
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+
+    user = await get_by_username(db_session=db_session, username=username)
+    if not user:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="User not found")
+
+    return user
+
+CurrentUser = Annotated[User, Depends(get_current_user)]
