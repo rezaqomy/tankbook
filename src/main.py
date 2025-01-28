1b3e04097244e4f352869b745e0b5326d894a891
reza <rezaqomyalasl@gmail.com>
2025-01-28 19:54:06 +0330

    add description to app

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from src.auth.views import user_router, auth_router
 
-app = FastAPI()
+app = FastAPI(
+    title="FastAPI JWT Authentication",
+    description="A simple FastAPI app with JWT authentication",
+    version="1.0.0",
+)
+
 
 
 app.include_router(auth_router)
