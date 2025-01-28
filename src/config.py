f528575d781c0fc3897b509b562e0c67bf15b319
reza <rezaqomyalasl@gmail.com>
2025-01-28 19:23:54 +0330

    rename Consts

--- src/config.py
+++ src/config.py
@@ -4,9 +4,9 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     APP_ENV: str = "development"
     DEBUG: bool = False
-    DISPATCH_JWT_SECRET: str
-    DISPATCH_JWT_ALG: str
-    DISPATCH_JWT_EXP: int
+    JWT_SECRET: str
+    JWT_ALG: str
+    JWT_EXP: int
 
     class Config:
         env_file = ".env"
