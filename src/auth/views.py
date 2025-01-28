3b7db09e4a8833a3df0061abf6071227275ffd02
reza <rezaqomyalasl@gmail.com>
2025-01-28 19:53:41 +0330

    create model for auth login and me

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,19 +1,62 @@
-from fastapi import APIRouter, HTTPException, status
-from pydantic import ValidationError, error_wrappers
+from fastapi import APIRouter, Depends, HTTPException, security, status
+from fastapi.security.oauth2 import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import ValidationError
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.status import HTTP_401_UNAUTHORIZED
 
 
-from src.database.core import DbSession
-from .service import create_user as service_create_user
-from .models import User, UserRead, UserCreate
-
+from src.database.core import DbSession, get_db
+from .service import (
+    CurrentUser,
+    get_by_username,
+    create_user as service_create_user,
+)
+from .models import (
+        User,
+        UserRead,
+        UserCreate,
+        UserLogin,
+        UserLoginResponse,
+        BearerToken,
+)
 
 
 auth_router = APIRouter(prefix="/auth")
 user_router = APIRouter(prefix="/user")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
-
-@user_router.post("/users/", response_model=UserRead)
+@user_router.post("/", response_model=UserRead)
 async def create_user_view(user: UserCreate, db: DbSession):
     new_user = await service_create_user(user=user, db_session=db)
     return new_user
+
+
+
+@auth_router.get("/me", response_model=UserRead)
+async def get_me(
+    user: CurrentUser,
+):
+    return user
+
+    
+
+
+@auth_router.post("/login", response_model=UserLoginResponse)
+async def login_user(
+    user_in: UserLogin,
+    db_session: DbSession
+):
+    user = await get_by_username(db_session=db_session, username=user_in.username)
+    if user and user.verify_password(user_in.password):
+        return {"token": user.token}
+
+    
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=[
+            {"loc": ["username"], "msg": "Invalid username.", "type": "value_error"},
+            {"loc": ["password"], "msg": "Invalid password.", "type": "value_error"},
+        ],
+    )
