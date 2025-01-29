ceaf88f4a1923edb092c269586e15e3554a2e6f4
reza <rezaqomyalasl@gmail.com>
2025-01-29 12:28:43 +0330

    create service for update customer

--- src/profile/service.py
+++ src/profile/service.py
@@ -1,8 +1,10 @@
 
+from datetime import UTC
+from fastapi import HTTPException
 from src.enums import SubscriptionModel
 from src.database.core import DbSession
 from src.auth.service import create_user
-from .models import Customer, CustomerRegister
+from .models import Customer, CustomerRegister, CustomerUpdate
 
 class CustomerService:
     @staticmethod
@@ -14,5 +16,18 @@ class CustomerService:
         await db_session.refresh(customer_db)
         return customer_db
 
+    @staticmethod
+    async def update(*, user_id: int, customer_update: CustomerUpdate, db_session: DbSession) -> Customer:
+        customer = await db_session.get(Customer, user_id)
+        if not customer:
+            raise HTTPException(status_code=404, detail="Customer not found")
 
+        update_data = customer_update.dict(exclude_unset=True)
 
+        for key, value in update_data.items():
+            setattr(customer, key, value)
+
+        await db_session.commit()
+        await db_session.refresh(customer)
+
+        return customer
