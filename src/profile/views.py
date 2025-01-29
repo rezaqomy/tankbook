349d57da7d9f9706602617cbe456546d2a439c75
reza <rezaqomyalasl@gmail.com>
2025-01-29 12:28:59 +0330

    crate view for get customer and update cusomter

--- src/profile/views.py
+++ src/profile/views.py
@@ -1,18 +1,27 @@
-from fastapi import APIRouter
+from typing import List
+from fastapi import APIRouter, HTTPException
+from sqlalchemy.sql import select
 
 from src.auth.models import UserRead
 from src.auth.service import get_by_id
 from src.database.core import DbSession
-from .models import CustomerRead, CustomerRegister
+from .models import CustomerGet, CustomerRead, CustomerRegister, CustomerUpdate, Customer, CustomerUpdateResponse
 from .service import CustomerService
 
 
 profile_route = APIRouter(prefix="/profile", tags=["profile", "customer"])
 
 
+@profile_route.get("/customers", response_model=List[CustomerGet])
+async def get_all_customers(db: DbSession):
+    # Asynchronous query
+    result = await db.execute(select(Customer))
+    customers = result.scalars().all()
+    return customers
+
 @profile_route.post('/customer', response_model=CustomerRead)
 async def create_customer_view(
-    customer: CustomerRegister,
+    customer: CustomerRead,
     db_session: DbSession,
     ):
     new_customer = await CustomerService.create(customer=customer, db_session=db_session)
@@ -22,3 +31,35 @@ async def create_customer_view(
         "subscription_end": new_customer.subscription_end,
         "wallet_money": new_customer.wallet_money
     }
+
+@profile_route.get("/customer/{customer_id}", response_model=CustomerGet)
+async def get_one_customer(customer_id: int, db: DbSession):
+    # Asynchronous query to get a single customer
+    result = await db.execute(select(Customer).filter(Customer.user == customer_id))
+    customer = result.scalars().first()
+
+    if not customer:
+        raise HTTPException(status_code=404, detail="Customer not found")
+    
+    return customer
+
+
+
+@profile_route.patch('/customer/{user_id}', response_model=CustomerUpdateResponse)
+async def update_customer_view(
+    user_id: int,
+    customer_update: CustomerUpdate,
+    db_session: DbSession,
+):
+    customer = await db_session.get(Customer, user_id)
+    if not customer:
+        raise HTTPException(status_code=404, detail="Customer not found")
+
+    update_data = customer_update.dict(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(customer, key, value)
+
+    await db_session.commit()
+    await db_session.refresh(customer)
+
+    return customer
