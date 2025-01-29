1401947685ad7ccd02862099484f8eac8e711185
reza <rezaqomyalasl@gmail.com>
2025-01-30 02:35:01 +0330

    update the models

--- src/book/models.py
+++ src/book/models.py
@@ -1,18 +1,25 @@
 
 
+from typing import Optional, Any, List
+from pydantic import Field
+from pydantic.utils import GetterDict
 from sqlalchemy import Column, ForeignKey, Integer, String, null
 from sqlalchemy.orm import relationship
 from sqlalchemy.util import FastIntFlag
 from src.base import PrimaryKeyMixin, TimeStampMixin
+from src.schemas import BookTankBase
 from src.database.core import Base
+from sqlalchemy.ext.associationproxy import association_proxy
 
-
-class BookAuthor(Base, PrimaryKeyMixin):
+class BookAuthor(Base):
     book_id = Column(ForeignKey('book.id'), primary_key=True)
     author_id = Column(ForeignKey('author.user_id'), primary_key=True)
     blurb = Column(String, nullable=False)
-    book = relationship("Book", back_populates="author")
-    author = relationship("Author", back_populates="book")
+    book = relationship("Book", back_populates="authors")
+    author = relationship("Author", back_populates="books")
+
+    author_name = association_proxy(target_collection='author', attr='name')
+    book_title = association_proxy(target_collection='book', attr='title')
 
 class Gener(Base, PrimaryKeyMixin):
     name = Column(String, nullable=False)
@@ -22,5 +29,40 @@ class Book(Base, PrimaryKeyMixin, TimeStampMixin):
     isbn = Column(String(13), nullable=False, unique=True)
     price = Column(Integer, nullable=False, default=0)
     description = Column(String(2056), nullable=True)
-    unit = Column(Integer, default=0)
+    unit = Column(Integer, default=0, nullable=False)
+
     authors = relationship("BookAuthor", back_populates="book")
+
+class BookCreateSchema(BookTankBase):
+    title: str = Field(..., max_length=255)
+    isbn: str = Field(..., max_length=13)
+    price: int = Field(..., ge=0)
+    description: Optional[str] = Field(None, max_length=2056)
+    unit: int = Field(0, ge=0)
+    author_ids: List[int]
+    blurbs: List[str]
+
+class AuthorResponse(BookTankBase):
+    user_id: int
+    city: Optional[int]
+    bank_number: Optional[str]
+
+    class Config:
+        orm_mode = True
+
+class BookAuthorResponse(BookTankBase):
+    author: AuthorResponse
+    blurb: str
+    book_title: str
+
+    class Config:
+        orm_mode = True
+
+class BookResponse(BookTankBase):
+    id: int
+    title: str
+    isbn: str
+    price: int
+    description: Optional[str]
+    unit: int
+    authors: List[BookAuthorResponse]
