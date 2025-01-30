3174717f73212ee9ac686b9e729035f4d120d831
reza <rezaqomyalasl@gmail.com>
2025-01-30 14:21:45 +0330

    create permission dependency

---
+++ src/auth/dependencies.py
@@ -0,0 +1,19 @@
+from fastapi import Depends, Request
+from .permissions import AdminPermission, CurrentUserOrAdminPermission, CurrentUserPermission
+from .service import CurrentUser, get_current_user
+from .models import User
+
+async def admin_permission(request: Request, user: CurrentUser):
+    permission = AdminPermission(request=request, user=user)
+    permission()
+    return user
+
+async def current_user(request: Request, user: CurrentUser):
+    permission = CurrentUserPermission(request=request, user=user)
+    permission()
+    return user
+
+async def current_user_or_admin(request: Request, user: CurrentUser):
+    permission = CurrentUserOrAdminPermission(request=request, user=user)
+    permission()
+    return user
---
+++ src/auth/permissions.py
@@ -0,0 +1,54 @@
+
+from fastapi import HTTPException, Request
+from starlette.status import HTTP_400_BAD_REQUEST, HTTP_403_FORBIDDEN
+from abc import ABC, abstractmethod
+from .models import UserRoles, User
+
+
+def any_permission(permissions: list, request: Request) -> bool:
+    for p in permissions:
+        try:
+            p(request=request)
+            return True
+        except HTTPException:
+            pass
+    return False
+
+class BasePermission(ABC):
+    def __init__(self, request: Request, user: User):
+        self.request = request
+        self.user = user
+        self.role = user.role
+
+    @abstractmethod
+    def has_permission(self) -> bool:
+        pass
+
+    def __call__(self) -> bool:
+        if not self.has_permission():
+            raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail="Not enough permissions")
+        return True
+
+
+class AdminPermission(BasePermission):
+    def has_permission(self) -> bool:
+        return self.role == UserRoles.ADMIN
+
+class CurrentUserPermission(BasePermission):
+    def __init__(self, request: Request,user: User):
+        self.user_id = int(request.path_params['user_id'])
+        self.user = user
+
+    def has_permission(self) -> bool:
+        is_current_user = self.user.id == self.user_id
+        return is_current_user 
+
+class CurrentUserOrAdminPermission(BasePermission):
+    def __init__(self, request: Request, user: User):
+        self.user = user
+        self.user_id = int(request.path_params['user_id'])
+
+
+    def has_permission(self) -> bool:
+        return any([self.user.id == self.user_id, self.user.role == UserRoles.ADMIN])
+
