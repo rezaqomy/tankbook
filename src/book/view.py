1340d45e1bcfbf2842a06e5e2612eed8fb1f6b1e
reza <rezaqomyalasl@gmail.com>
2025-01-30 03:38:11 +0330

    create view for get book by id and get all book

--- src/book/view.py
+++ src/book/view.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import joinedload
 
-from .service import BookService
+from .service import BookService, get_by_isbn
 from src.database.core import DbSession
 from .models import Book, BookCreateSchema, BookResponse
 
@@ -14,20 +14,12 @@ book_router = APIRouter(prefix="/book", tags=["book"])
 async def create_book(book_data: BookCreateSchema, db: DbSession):
     book = await BookService.create_book(db, book_data, book_data.author_ids, book_data.blurbs)
     return book
-
-
 @book_router.get("/{id}", response_model=BookResponse)
 async def get_book(id: int, db: DbSession):
-    result = await db.execute(select(Book).options(joinedload(Book.authors)).filter(Book.id == id))
-    db_book = result.scalars().first()  # Use scalars().first() instead of scalar_one_or_none()
-    
-    if db_book is None:
-        raise HTTPException(status_code=404, detail="Book not found")
-    
+    db_book = await BookService.get_book(db, id)
     return db_book
 
 @book_router.get("/", response_model=List[BookResponse])
 async def get_books(db: DbSession):
-    result = await db.execute(select(Book).options(joinedload(Book.authors)))
-    db_books = result.scalars().all()  # Get all records (books)
+    db_books = await BookService.get_all_books(db)
     return db_books
